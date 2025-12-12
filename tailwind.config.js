bab2fa0494e479206fbfd741d10f00ca7d5886fa
Ashwin Balaji <ashwinbalaji@Ashwins-MacBook-Pro.fritz.box>
2025-12-12 17:50:14 +0100

    creating problems is the name of the game v8

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import { createPreset } from 'fumadocs-ui/tailwind-plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
